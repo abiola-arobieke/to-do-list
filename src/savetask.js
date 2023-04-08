5d4e53c87a891632e11d63a6ebd9b9ca00d7ea2a
abiola <abiola.arobieke@gmail.com>
2023-04-08 07:58:08 -0400

    Add savetask.js to the project

---
+++ src/savetask.js
@@ -0,0 +1,12 @@
+export const task = JSON.parse(localStorage.getItem('allTask')) || [];
+export const todoTask = JSON.parse(localStorage.getItem('todo-id')) || {};
+
+// save task id
+export const saveTaskId = () => {
+  localStorage.setItem('todo-id', JSON.stringify(todoTask));
+};
+
+// update task array
+export const saveTask = () => {
+  localStorage.setItem('allTask', JSON.stringify(task));
+};
